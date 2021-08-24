df3bf4ed40a07f6e91ddf76caa02b0a7d1cd8b1a
Marcos Mantovani <mantovani.marcos@gmail.com>
2021-08-24 19:58:31 -0300

    changing scale and border radius in the anim

--- src/app.js
+++ src/app.js
@@ -4,25 +4,32 @@ let ol = 0;
 let circleInterval;
 let isPlaying = false;
 
-document.getElementById("circle").style.backgroundColor = 'dimgrey'
+const circle = document.getElementById("circle")
+let scale = 1
+
+circle.style.backgroundColor = 'dimgrey'
 
 document.getElementById('circle')
-    .addEventListener('click', mouseClicked)
+   .addEventListener('click', mouseClicked)
 
 function circleAnimation(time) {
     ol++
-    document.getElementById("circle").style.left = ol + "px"
+    scale++
+    circle.style.left = ol + 'px'
+    circle.style.width = scale + 'px'
+    circle.style.height = scale + 'px'
+    circle.style.borderRadius = scale * 10 + 'px'
     circleInterval = requestAnimationFrame(circleAnimation)
 }
 
 function mouseClicked() {
     if(isPlaying) {
         isPlaying = false
-        document.getElementById("circle").style.backgroundColor = 'dimgrey'
+        circle.style.backgroundColor = 'dimgrey'
         cancelAnimationFrame(circleInterval)
     } else {
         isPlaying = true
-        document.getElementById("circle").style.backgroundColor = 'crimson'
+        circle.style.backgroundColor = 'crimson'
         circleInterval = requestAnimationFrame(circleAnimation)
     }
 }
