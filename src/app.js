3999e824668729d9063ff4c343e4a3b7a1d505ce
Marcos Mantovani <mantovani.marcos@gmail.com>
2021-08-24 19:53:16 -0300

    simple animation control

--- src/app.js
+++ src/app.js
@@ -2,19 +2,29 @@ import './app.css'
 
 let ol = 0;
 let circleInterval;
+let isPlaying = false;
+
+document.getElementById("circle").style.backgroundColor = 'dimgrey'
+
+document.getElementById('circle')
+    .addEventListener('click', mouseClicked)
 
 function circleAnimation(time) {
     ol++
     document.getElementById("circle").style.left = ol + "px"
+    circleInterval = requestAnimationFrame(circleAnimation)
+}
 
-    if(time < 4000){
-        circleInterval = requestAnimationFrame(circleAnimation)
+function mouseClicked() {
+    if(isPlaying) {
+        isPlaying = false
+        document.getElementById("circle").style.backgroundColor = 'dimgrey'
+        cancelAnimationFrame(circleInterval)
     } else {
-        document
-            .getElementById("circle")
-            .style.backgroundColor = "dimgrey"
-            cancelAnimationFrame(circleInterval)
+        isPlaying = true
+        document.getElementById("circle").style.backgroundColor = 'crimson'
+        circleInterval = requestAnimationFrame(circleAnimation)
     }
 }
 
-circleInterval = requestAnimationFrame(circleAnimation)
+
