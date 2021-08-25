da1772752023cc477517e6fd5ea3414cdacea8b1
Marcos Mantovani <mantovani.marcos@gmail.com>
2021-08-25 00:46:46 -0300

    easing formulas applied to animation

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import './app.css'
+import "./app.css"
 
 /*
 let ol = 0;
@@ -38,27 +38,70 @@ function mouseClicked() {
 /**
  * CANVAS CODE
  */
-const canvas = document.getElementById('animated-canvas')
-const context = canvas.getContext('2d')
+const canvas = document.getElementById("animated-canvas")
+const context = canvas.getContext("2d")
 let circleX = 60
 
+/**
+ * Interpolation with 0..1 range
+ * http://sol.gfxile.net/interpolation/
+ *
+ */
+
+/*********** Easing filters  ********************/
+const SMOOTHSTEP = (x) => (x) * (x) * (3 - 2 * (x))
+const SMOOTHERSTEP = (x) => ((x) * (x) * (x) * ((x) * ((x) * 6 - 15) + 10))
+const SIMPLEPOWER = (x) => x * x // ease-in
+const SIMPLEPOWERREVERSE = (x) => 1 - (1 - x) * (1 - x) // ease-out
+
+/*********** / Easing filters  ********************/
+
+const steps = 100
+const origin = 10
+const destination = 590
+
+let i = 0
+
 function canvasAnimation() {
-    circleX++
-    drawCircle(circleX, canvas.height/2, 25)
-    canvasInterval = requestAnimationFrame(canvasAnimation)
+  i++
+  i = i % steps
+
+  drawCircle(
+    generalInterpolation(origin, destination, i, steps, SIMPLEPOWERREVERSE)
+    , canvas.height / 2, 25
+  )
+
+  // stop animation if i is not repeating, comment steps modulus  applied
+  if (i == steps) {
+    cancelAnimationFrame(canvasInterval)
+    return
+  }
+
+  canvasInterval = requestAnimationFrame(canvasAnimation)
+}
+
+function generalInterpolation(origin, destination, currTime, steps, easing) {
+
+  let velocity = currTime / steps
+
+  // velocity is the changing rate
+  if (easing !== undefined) {
+    velocity = easing(velocity)
+  }
+
+  return destination * velocity + origin * (1 - velocity)
 }
 
 function drawCircle(x, y, rad) {
-    clearCanvas()
-    context.fillStyle = 'crimson'
-    context.beginPath()
-    context.arc(x, y, rad, 0, 2 * Math.PI)
-    context.fill()
+  clearCanvas()
+  context.fillStyle = "crimson"
+  context.beginPath()
+  context.arc(x, y, rad, 0, 2 * Math.PI)
+  context.fill()
 }
 
 function clearCanvas() {
-    context.clearRect(0, 0, canvas.clientWidth, canvas.height)
+  context.clearRect(0, 0, canvas.clientWidth, canvas.height)
 }
-
 
 let canvasInterval = requestAnimationFrame(canvasAnimation)
