4f6dba45a4d67433c240061008dd4b49e38cf061
Marcos Mantovani <mantovani.marcos@gmail.com>
2021-08-24 23:44:54 -0300

    animating using a canvas conteiner

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import './app.css'
 
+/*
 let ol = 0;
 let circleInterval;
 let isPlaying = false;
@@ -32,6 +33,32 @@ function mouseClicked() {
         circle.style.backgroundColor = 'crimson'
         circleInterval = requestAnimationFrame(circleAnimation)
     }
+} */
+
+/**
+ * CANVAS CODE
+ */
+const canvas = document.getElementById('animated-canvas')
+const context = canvas.getContext('2d')
+let circleX = 60
+
+function canvasAnimation() {
+    circleX++
+    drawCircle(circleX, canvas.height/2, 25)
+    canvasInterval = requestAnimationFrame(canvasAnimation)
+}
+
+function drawCircle(x, y, rad) {
+    clearCanvas()
+    context.fillStyle = 'crimson'
+    context.beginPath()
+    context.arc(x, y, rad, 0, 2 * Math.PI)
+    context.fill()
+}
+
+function clearCanvas() {
+    context.clearRect(0, 0, canvas.clientWidth, canvas.height)
 }
 
 
+let canvasInterval = requestAnimationFrame(canvasAnimation)
